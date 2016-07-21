773cf194b07ca9d1cf924844ac1a8265b5aaa9d6
kishvadde <kishorekumar.vadde555@gmail.com>
2016-07-21 09:18:50 +0530

    Update extract_feature.py

--- extract_feature.py
+++ extract_feature.py
@@ -9,7 +9,7 @@ def extract_features(content, min_len=2, max_len=20):
     words = []       
     pattern = r'''(?x)([A-Z]\.)+|\w+(-\w+)*|\$?\d+(\.\d+)?%?| \.\.\.| [][.,;"'?():-_`]'''  	#regular expression to tokenize    
     tokens = nltk.regexp_tokenize(content,pattern)   
-    words = [unicode(w.lower(),encoding='latin-1') for w in tokens]  #convert all letters to lowercase   
+    words = [unicode(w.lower(),encoding='latin-1') for w in tokens]      #convert all letters to lowercase   
     imp_words = [w for w in words if w.lower() not in stopwords.words('english')] 		#Remove stopwords    
     poster = nltk.PorterStemmer()    
     words = [poster.stem(w) for w in imp_words if min_len<=len(w)<=max_len]   			#stem the features   
